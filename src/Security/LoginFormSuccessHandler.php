0fd64a78c33ee27a4b4a9d8b1ca37c9c905974b6
ZeroSTF <kateskatous@gmail.com>
2023-04-28 09:43:18 +0100

    fixes

--- src/Security/LoginFormSuccessHandler.php
+++ src/Security/LoginFormSuccessHandler.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
 class LoginFormSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
     private $router;
     private $authorizationChecker;
+    private Security $security;
 
     public function __construct(RouterInterface $router, AuthorizationCheckerInterface $authorizationChecker)
     {
@@ -23,11 +25,11 @@ class LoginFormSuccessHandler implements AuthenticationSuccessHandlerInterface
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
         if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
-            
-            return new RedirectResponse($this->router->generate('app_user_index'));
+
+            return new RedirectResponse($this->router->generate('app_admin'));
         }
 
         // redirect to the default page for other users
-        return new RedirectResponse($this->router->generate('app_index'));
+        return new RedirectResponse($this->router->generate('app_front'));
     }
 }
